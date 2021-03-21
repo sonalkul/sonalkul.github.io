6cc610a0b101d0b845ec44076d1d99a24215a54c
sonalkul <81103143+sonalkul@users.noreply.github.com>
2021-03-21 22:26:10 +0530

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,24 @@
+var userid, description, addBtn, msg, email;
+
+
+userid = document.getElementById("userName").value;
+
+addBtn = document.getElementById("addBtn");
+
+
+const database = firebase.database();
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    userid = document.getElementById("userName").value;
+    email = document.getElementById("useremail").value;
+    msg = document.getElementById("usermsg").value
+    if (userid !== ""){
+
+    database.ref('/users/userid'+userid).set({
+        username: userid,
+        emailId: email,
+        message: msg,
+    });
+}
+});
